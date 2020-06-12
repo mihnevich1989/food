5186e09a73a4f0f719779afdcfcbecc762e5e805
mikhnevich <mikhnevich.work@mail.ru>
2020-06-12 20:36:53 +0300

    изменил XMLHttpRequest на fetch

--- js/script.js
+++ js/script.js
@@ -247,29 +247,26 @@ document.addEventListener('DOMContentLoaded', () => {
 			`;
 			form.insertAdjacentElement('afterend', statusMessage);
 
-			const request = new XMLHttpRequest();
-			request.open('POST', 'server.php');
-
-			request.setRequestHeader('Content-type', 'application/json');
 			const formData = new FormData(form);
 
 			const object = {};
 			formData.forEach((value, key) => {
 				object[key] = value;
 			});
-			const json = JSON.stringify(object);
 
-			request.send(json);
-			request.addEventListener('load', () => {
-				if (request.status === 200) {
-					console.log(request.response);
-					showThanksModal(message.success);
-					form.reset();
-					statusMessage.remove();
-				} else {
-					showThanksModal(message.failure);
-				}
-			});
+			fetch('server.php', {
+				method: 'POST',
+				headers: { 'Content-type': 'application/json' },
+				body: JSON.stringify(object)
+			}).then(data => data.text()).then(data => {
+				console.log(data);
+				showThanksModal(message.success);
+				statusMessage.remove();
+			}).catch(() => {
+				showThanksModal(message.failure);
+			}).finally(() => {
+				form.reset();
+			})
 		});
 	}
 	function showThanksModal(message) {
@@ -293,23 +290,5 @@ document.addEventListener('DOMContentLoaded', () => {
 			closeModal();
 		}, 4000);
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 });
